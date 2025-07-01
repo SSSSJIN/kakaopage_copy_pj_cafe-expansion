21c7e37953e59c9d51f9464b7cf9005ca9ed2e83
SSSSJIN <shoes90@naver.com>
2025-07-01 21:21:03 +0900

    07012119_after-ppt_backup_KakaopageExpansionProject
    view- alive!!!
    발표 후 백업 파일
    다음 업데이트 기한 :
    ~ 07.12

--- cafe-expansion/src/main/java/com/kakaopage/expansion/vo/EpisodeVO.java
+++ cafe-expansion/src/main/java/com/kakaopage/expansion/vo/EpisodeVO.java
@@ -3,21 +3,36 @@ package com.kakaopage.expansion.vo;
 import java.time.LocalDateTime;
 
 public class EpisodeVO {
+
     private Long id;
     private Long bookId;
     private String title;
     private int episodeNo;
     private LocalDateTime regDate;
 
+    // content 필드는 viewer.jsp 등에서 ${ep.content} 접근시 500에러 방지용
+    private String content;
+
     // Getter/Setter
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public Long getBookId() { return bookId; }
     public void setBookId(Long bookId) { this.bookId = bookId; }
+
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
     public int getEpisodeNo() { return episodeNo; }
     public void setEpisodeNo(int episodeNo) { this.episodeNo = episodeNo; }
+
     public LocalDateTime getRegDate() { return regDate; }
     public void setRegDate(LocalDateTime regDate) { this.regDate = regDate; }
+
+    // detail.jsp에서 ${ep.date}로 접근할 수 있도록
+    public LocalDateTime getDate() { return regDate; }
+
+    // viewer.jsp 등에서 ${ep.content} 접근시 500에러 방지
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
 }
